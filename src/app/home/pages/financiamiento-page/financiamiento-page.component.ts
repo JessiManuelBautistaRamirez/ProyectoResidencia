40005d4b60f151f285a28c650053181987347069
Jessi <l19070004@huetamo.tecnm.mx>
2024-10-09 21:55:39 -0600

    Avance

--- src/app/home/pages/financiamiento-page/financiamiento-page.component.ts
+++ src/app/home/pages/financiamiento-page/financiamiento-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-financiamiento-page',
   templateUrl: './financiamiento-page.component.html',
-  styles: ``
+  styleUrls: ['./style.css']
 })
 export class FinanciamientoPageComponent {
 
